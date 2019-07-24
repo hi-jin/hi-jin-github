3c7eac6494d1621606c5eb79719a1c835bc0e897
hi-jin <crushed7@o.cnu.ac.kr>
2019-07-24 23:18:16 +0900

    ADD - CNU information

---
+++ assets/js/menu4.js
@@ -0,0 +1,10 @@
+document.write('<nav id="menu">');
+  document.write('<h2>Menu</h2>');
+  document.write('<ul>');
+    document.write('<li><a href="../../../index.html">Home</a></li>');
+    document.write('<li><a href="../../../programming/index.html">Programming</a></li>');
+    document.write('<li><a href="../../../siliconvalley/index.html">SiliconValley review</a></li>');
+    document.write('<li><a href="../../../mogaco/index.html">Mogaco</a></li>');
+    document.write('<li><a href="../../../lol/chess/index.html">LOL Chess</a></li>');
+  document.write('</ul>');
+document.write('</nav>');
