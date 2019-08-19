cff6a1648d0aa0acd60f1931e398bdc296fde8eb
hi-jin <crushed7@o.cnu.ac.kr>
2019-08-19 16:31:46 +0900

    ADD - raspberry pi / web / setting raspberry pi

--- assets/js/menu3.js
+++ assets/js/menu3.js
@@ -6,5 +6,6 @@ document.write('<nav id="menu">');
     document.write('<li><a href="../../siliconvalley/index.html">SiliconValley review</a></li>');
     document.write('<li><a href="../../mogaco/index.html">Mogaco</a></li>');
     document.write('<li><a href="../../lol/chess/index.html">LOL Chess</a></li>');
+    document.write('<li><a href="../../raspberry/index.html">Raspberry Pi</a></li>');
   document.write('</ul>');
 document.write('</nav>');
